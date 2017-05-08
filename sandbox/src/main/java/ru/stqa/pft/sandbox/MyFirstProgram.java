142af1e6153d393bbc05560b7dbbf32f967a25b9
leszek86 <tomek8686@gmail.com>
2017-05-08 21:09:36 +0200

    Added test for Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,24 +7,15 @@ public class MyFirstProgram {
 		hello("Tomasz");
 
 		Square s = new Square(5);
-		System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + area(s));
+		System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area());
 
 		Rectangle r = new Rectangle(4,6);
 		
-		System.out.println("Powierzchnia prostokata o bokach " + r.a + " oraz " + r.b + " = " + area(r));
+		System.out.println("Powierzchnia prostokata o bokach " + r.a + " oraz " + r.b + " = " + r.area());
 	}
 
 	public static void hello(String somebody){
 		System.out.println("Hello " + somebody + " !");
 	}
-	
-	public static double area(Square s) {
 
-		return s.l * s.l;
-	}
-	
-	public static double area(Rectangle r){
-
-		return r.a * r.b;
-	}
 }
